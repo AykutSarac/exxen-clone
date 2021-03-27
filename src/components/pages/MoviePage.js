6a8ef6f207ee02bc6e38f65319e6b36de55e2389
AykutSarac <aykutsarac0@gmail.com>
2021-03-27 20:45:55 +0300

    pages made responsive

--- src/components/pages/MoviePage.js
+++ src/components/pages/MoviePage.js
@@ -7,8 +7,12 @@ const divStyle = css`
     justify-content: center;
     align-items: baseline;
     width: 70%;
-    margin: 15px auto;
+    margin: 15px auto 3em auto;
     height: 100%;
+
+    @media screen and (max-width: 768px) {
+        flex-direction: column;
+    }
 `
 
 const PosterContent = styled.div`
@@ -16,6 +20,20 @@ const PosterContent = styled.div`
     display: inline-block;
     min-width: 21%;
     width: 21%;
+
+    @media screen and (max-width: 900px) {
+        order: 2;
+        width: 100%;
+
+        h1 {
+            text-align: center;
+        }
+    }
+`
+
+const h1Text = css`
+    margin: 12px 0;
+    color: white;
 `
 
 const InfoBox = styled.div`
@@ -26,6 +44,23 @@ const InfoBox = styled.div`
 const Fragman = styled.div`
     width: 100%;
     margin: 2em;
+
+    @media screen and (max-width: 900px) {
+        order: 1;
+    }
+`
+
+const frame = css`
+    height: 85%;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    border: none;
+    padding-bottom: 2em;
+    
 `
 
 const MoviePage = ({ match }) => {
@@ -40,7 +75,7 @@ const MoviePage = ({ match }) => {
     return (
         <div css={divStyle}>
             <PosterContent>
-                <h1 style={{ color: 'white', margin: '12px 0' }}>{movie.name}</h1>
+                <h1 css={h1Text}>{movie.name}</h1>
                 <img src={movie.poster} alt="poster" style={{ width: '100%', margin: '0', display: 'block' }} />
                 <InfoBox>
                     {movie.type ? movie.type : "Bilinmiyor"}
@@ -49,7 +84,7 @@ const MoviePage = ({ match }) => {
             <Fragman>
                 <h2 className="active">Fragman</h2>
                 <div className="video" style={{ overflow: 'hidden', position: 'relative', paddingTop: '52%' }}>
-                    <iframe style={{ height: '85%', width: '100%', position: 'absolute', left: '0', top: '0', right: '0', bottom: '0' }} src={movie.fragman} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <iframe css={frame} src={movie.fragman} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <p style={{ color: 'white', marginTop: '2em', position: 'relative' }}>
                         {movie.info ? movie.info : 'Geçerli bir açıklama bulunamadı.'}
                     </p>
